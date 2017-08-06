3f1ead9c78ad5473e2b673e984e7b61befa1661a
Ionuț Bogdan <ionutbogdan91@gmail.com>
2017-08-06 22:11:46 +0300

    This is kinda a brute force aproach but it works :). (i hope to refactor the code in the next days)

---
+++ Tema3/src/main/java/ro/ionutbogdan/homeworks/SalesCompany.java
@@ -0,0 +1,11 @@
+package ro.ionutbogdan.homeworks;
+
+/**
+ * The Sales Company class is created for a possible extension of the homework (what if we have more companys?)
+ *
+ * @author Ionut Bogdan
+ */
+public class SalesCompany {
+    String name;
+    int numberOfDepartments;
+}
---
+++ Tema3/src/main/java/ro/ionutbogdan/homeworks/SalesDepartment.java
@@ -0,0 +1,11 @@
+package ro.ionutbogdan.homeworks;
+
+/**
+ * We created the SalesDepartment class so we can easely work with the departments.
+ *
+ * @author Ionut Bogdan
+ */
+public class SalesDepartment {
+    String name;
+    int numberOfSalesPersons;
+}
